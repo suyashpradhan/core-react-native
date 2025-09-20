41ec4322cb556ac05455c37b4b2ba0afcd37352a
Suyash Pradhan <pradhangroup@hotmail.com>
2025-09-20 10:09:11 +0530

    added safe area provider and insets

--- App.tsx
+++ App.tsx
@@ -1,26 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaProvider >
+    <SafeAreaView edges={['bottom']} style={styles.container}>
+      <Image source={{uri:'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg'}} style={styles.image}/>
       <Image source={require('./assets/adaptive-icon.png')} style={styles.logo} />
-      <Text>Hello World!</Text>
+      <Text style={styles.text}> Hello World!</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    backgroundColor: 'black',
+alignItems:'center',  
+},
   logo: {
-    width: 300,
-    height: 300,
+    width: 150,
+    height: 150,
+    borderWidth: 5,
+    borderColor: 'white',
+    marginTop: -75,
+    borderRadius:100
   },
+  image:{
+    width: '100%',
+    aspectRatio: 16/9,
+  },
+  text:{
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+  }
 });
